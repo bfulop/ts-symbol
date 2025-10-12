a19b4fb8dbb8650179218e8e683d1d98646d1cbd
Balint Fulop <bfulop@mac.com>
2025-10-12 16:42:10 +0200

    larger scope matching

--- ast-grep-playground/src/calls/call-in-await.ts
+++ ast-grep-playground/src/calls/call-in-await.ts
@@ -1,4 +1,11 @@
 /* call with await */
 async function g() {
-  await foo(5)
+  await foo(5);
+}
+
+/* call with await */
+async function h() {
+  return async function bar() {
+    await foo();
+  };
 }
